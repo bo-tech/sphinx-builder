d732dd56ec47ce507dae75bd94e8a4f727e56751
Johannes Bornhold <johannes@bornhold.name>
2025-11-19 21:26:55 +0100

    feat: Add flake configuration

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Sphinx builder";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        packages = import ./packages.nix { inherit pkgs; };
+      in
+      {
+        inherit packages;
+      }
+    );
+}
